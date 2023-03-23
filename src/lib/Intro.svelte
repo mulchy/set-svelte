7bfb5ff3806c7d116a484f7cb089eb69119fd04b
Ryan Mulcahy <mulchy81@gmail.com>
2023-03-23 14:36:00 -0500

    typo, formatting

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -4,33 +4,48 @@
 </script>
 
 <div class="container">
-    <h1>How to play <a href="https://en.wikipedia.org/wiki/Set_(card_game)">set</a></h1>
+    <h1>
+        How to play <a href="https://en.wikipedia.org/wiki/Set_(card_game)"
+            >set</a
+        >
+    </h1>
 
-    <p>Set is a card matching game. The goal is to identify a matching set as quickly as possible. Each card has 4 features: </p>
+    <p>
+        Set is a card matching game. The goal is to identify a matching set as
+        quickly as possible. Each card has 4 features:
+    </p>
     <ul>
-        <li>Color (<span style="color:red">red</span>, <span style="color:green">green</span>, <span style="color:purple">purple</span>)</li>
+        <li>
+            Color (<span style="color:red">red</span>,
+            <span style="color:green">green</span>,
+            <span style="color:purple">purple</span>)
+        </li>
         <li>Shape (round, diamond, squiggle)</li>
         <li>Fill (empty, dashed, solid)</li>
         <li>Number (1,2,3)</li>
     </ul>
-   
 
     <p>
-        Three cards form a <em>set</em> if each attribute is either all the same
-        or all different. For example the following three cards form a set since the colors are all different, the shapes are all the same, the fills are all different, and the numbers are the same.</p>
+        Three cards form a <em>set</em> if each attribute is either all the same,
+        or all different. For example the following three cards form a set since
+        the colors are all different, the shapes are all the same, the fills are
+        all different, and the numbers are the same.
+    </p>
     <div class="cards">
         <Card color="red" shape="squiggle" fill="empty" number={1} />
         <Card color="green" shape="squiggle" fill="dashed" number={1} />
         <Card color="purple" shape="squiggle" fill="solid" number={1} />
     </div>
 
-    <p>These three cards <em>do not</em> form a set since two of colors are green, but the third color is purple.</p>
+    <p>
+        These three cards <em>do not</em> form a set since two of the colors are
+        green, but the third color is purple.
+    </p>
     <div class="cards">
         <Card color="green" shape="squiggle" fill="empty" number={2} />
         <Card color="green" shape="squiggle" fill="dashed" number={2} />
         <Card color="purple" shape="squiggle" fill="solid" number={2} />
     </div>
-
 
     <button on:click={() => newGame(state)}>Start Game</button>
 </div>
@@ -69,5 +84,4 @@
     p {
         margin-bottom: 0;
     }
-
 </style>
