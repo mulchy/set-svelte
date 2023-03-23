d4bde8e85a16d9ad057b1fefd9fc7b28446a1b26
Ryan Mulcahy <mulchy81@gmail.com>
2023-03-23 14:29:05 -0500

    fix base, remove unused package

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import importAssets from 'svelte-preprocess-import-assets'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: [vitePreprocess(),importAssets()]
+  preprocess: vitePreprocess()
 }
